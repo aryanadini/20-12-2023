3fcc5c2166c388cf60cd8e53bf618fd65291f372
aryanadini <119577677+aryanadini@users.noreply.github.com>
2023-12-20 11:15:16 +0530

    Add files via upload

---
+++ lib/MyVerticalBalancePage.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class MyVerticalBalancePage extends StatefulWidget {
+  const MyVerticalBalancePage({super.key});
+
+  @override
+  State<MyVerticalBalancePage> createState() => _MyVerticalBalancePageState();
+}
+
+class _MyVerticalBalancePageState extends State<MyVerticalBalancePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
